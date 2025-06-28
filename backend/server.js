9c052c4da17dec22b021730326d8cc50e45bae5e
Anil Patidar <anilpatidar656@gmail.com>
2025-06-28 12:07:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
-
+const PORT = process.env.PORT || 6000;
 const userRouter = require("./routes/userRouter");
 const blogRouter = require("./routes/blogRouter")
 const app = express();
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URL, {
 .catch(err => console.error("MongoDB connection error:", err));
 
 
-const PORT = process.env.PORT || 6000;
+
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
